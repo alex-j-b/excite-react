d8c1ad4e77b5cbd18ca86b9892b2a71de10e5c17
Alex <darjeilolol@gmail.com>
2020-01-22 00:23:12 +0100

    first commit

---
+++ src/containers/Home.js
@@ -0,0 +1,11 @@
+import React from "react";
+import "./Home.css";
+
+export default function Home(props) {
+    return (
+        <div className="lander">
+            <h1>Scratch</h1>
+            <p>A simple note taking app</p>
+        </div>
+    );
+}
