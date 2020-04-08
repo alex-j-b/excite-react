66c06523d71c8b8a6fe342e479ce0d8534b054db
Alex <darjeilolol@gmail.com>
2020-04-08 21:38:47 +0200

    test reload service worker

--- src/serviceWorker.js
+++ src/serviceWorker.js
@@ -68,10 +68,6 @@ function registerValidSW(swUrl, config) {
         .catch(error => {
             console.error('Error during service worker registration:', error);
         });
-
-    navigator.serviceWorker.addEventListener('install', function() {
-        navigator.serviceWorker.skipWaiting();
-    });
 }
 
 export function unregister() {
